bbceb094935de1f8efcd62d5e04c3353931f5b81
Jigesh Vora <jigeshv@thinknear.com>
2014-09-11 13:27:11 -0700

    Initial commit

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+#RSpec.describe "StaticPages", :type => :request do
+#  describe "GET /static_pages" do
+#    it "works! (now write some real specs)" do
+#      get static_pages_index_path
+#      expect(response.status).to be(200)
+#    end
+#  end
+
+describe "Static Pages" do
+
+	describe "Home page" do
+		it "should have the content 'Sample App'" do
+			visit '/static_pages/home'
+			expect(page).to have_content('Sample App')
+		end	
+	end
+
+	describe "Help page" do
+
+		it "should have the content 'Help'" do
+			visit '/static_pages/help'
+			expect(page).to have_content('Help')
+		end
+	end		
+end
+
